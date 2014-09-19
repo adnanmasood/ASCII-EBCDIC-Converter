bbeef50cca8bffdd11be6b35af764047145cde36
Adnan Masood <adnanmasood@gmail.com>
2014-09-19 17:23:48 -0400

    initial check-in
    A Simple ASCII/EBCDIC Convetor Utility
    
    This utility can convert files TO/From ASCII and EBCDIC Encoding and do
    line breaks.

---
+++ code/Convertor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ASCII_EBCDIC_Converter
+{
+    internal class Convertor
+    {
+        public static void Convert(string inFile, string inputType, string outFile, bool crlf, int crlfLength,
+            string codepage)
+        {
+            byte[] fileArray = File.ReadAllBytes(inFile);
+
+            if (inputType.ToLower() == "ebcdic")
+                fileArray = ConvertAsciiToEbcdic(fileArray, codepage);
+            else if (inputType.ToLower() == "ascii")
+                fileArray = ConvertEbcdicToAscii(fileArray, codepage);
+            else
+                throw new Exception(
+                    "Unable to process the conversion type. Please check the converto parameter in the config file.");
+
+            using (var fileStream = new FileStream(outFile, FileMode.Create, FileAccess.Write))
+            {
+                int byteCount = 0;
+                int offset = 0;
+
+                if (crlf)
+                {
+                    var byteArray = new byte[crlfLength + Encoding.Default.GetBytes(Environment.NewLine).Length];
+                    foreach (byte b in fileArray)
+                    {
+                        byteArray[byteCount++] = b;
+                        if (byteCount%3000 == 0)
+                        {
+                            foreach (byte _byte in Encoding.Default.GetBytes(Environment.NewLine))
+                                byteArray[byteCount++] = _byte;
+                            fileStream.Write(byteArray, 0, byteArray.Length);
+                            offset += byteCount;
+                            byteCount = 0;
+                        }
+                    }
+                }
+                fileStream.Close();
+            }
+        }
+
+        #region public static byte[] ConvertAsciiToEbcdic(byte[] asciiData)
+
+        public static byte[] ConvertAsciiToEbcdic(byte[] asciiData, string codepage)
+        {
+            Encoding ascii = Encoding.ASCII;
+            Encoding ebcdic = Encoding.GetEncoding(codepage);
+            return Encoding.Convert(ascii, ebcdic, asciiData);
+        }
+
+        #endregion
+
+        #region public static byte[] ConvertEbcdicToAscii(byte[] ebcdicData)
+
+        public static byte[] ConvertEbcdicToAscii(byte[] ebcdicData, string codepage)
+        {
+            Encoding ascii = Encoding.ASCII;
+            Encoding ebcdic = Encoding.GetEncoding(codepage);
+            return Encoding.Convert(ebcdic, ascii, ebcdicData);
+        }
+
+        #endregion
+    }
+}
---
+++ code/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Configuration;
+using ASCII_EBCDIC_Converter;
+
+namespace ASCII_EBCIDIC_Converter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Process();
+        }
+
+        private static void Process()
+        {
+            #region banner
+            Console.WriteLine("\n=====================================================================");
+            Console.WriteLine("aec.exe - Simple ASCII / EBCDIC Convertor Util");
+            Console.WriteLine("Uses the application config file for values so make sure it's there.");
+            Console.WriteLine("Comments/Questions - adnan.masood@owasp.org");
+            Console.WriteLine("=====================================================================\n");
+            #endregion
+
+            try
+            {
+                string inFile;
+                string outFile;
+                string convertTo;
+                bool cRLF;
+                int bytesToSkipForCRLF;
+                string codePage;
+
+                GetConfigValues(out inFile, out outFile, out convertTo, out cRLF, out bytesToSkipForCRLF, out codePage);
+                Convertor.Convert(inFile, convertTo, outFile, cRLF, bytesToSkipForCRLF, codePage);
+                Console.WriteLine("\nOutput file written: " + outFile);
+                Console.WriteLine("\nAll Done. Bye now.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nOops, something broke!");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("\n");
+                Environment.Exit(-1);
+            }
+        }
+
+        private static void GetConfigValues(out string inFile, out string outFile, out string convertTo, out bool cRLF,
+            out int bytesToSkipForCRLF, out string codePage)
+        {
+            inFile = ConfigurationManager.AppSettings["sourcefilename"];
+            outFile = ConfigurationManager.AppSettings["outputfilename"];
+            convertTo = ConfigurationManager.AppSettings["convertto"];
+            cRLF = bool.Parse(ConfigurationManager.AppSettings["crlf"]);
+            bytesToSkipForCRLF = int.Parse(ConfigurationManager.AppSettings["skipbytesforcrlf"]);
+            codePage = ConfigurationManager.AppSettings["codepage"];
+        }
+    }
+}
